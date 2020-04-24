622331291a76c80b68c95aa8ec6eceea0fc4b48c
ManojKulkarni-HMCTS <61502234+ManojKulkarni-HMCTS@users.noreply.github.com>
2020-04-24 09:55:30 +0100

    Update SecurityConfiguration.java

--- src/main/java/uk/gov/hmcts/reform/pcqconsolidationservice/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pcqconsolidationservice/security/SecurityConfiguration.java
@@ -13,6 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
+        
         web.ignoring().antMatchers("/swagger-ui.html",
                 "/webjars/springfox-swagger-ui/**",
                 "/swagger-resources/**",
