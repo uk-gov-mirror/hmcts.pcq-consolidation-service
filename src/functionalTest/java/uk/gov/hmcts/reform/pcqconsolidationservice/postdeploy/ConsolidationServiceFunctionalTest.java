eaacc895dd7e6fac9fa4661aabe793cbd80597c9
Dave Jones <dave.jones@hmcts.net>
2020-05-05 14:11:34 +0100

    PCQ-181: Update credentials.

--- src/functionalTest/java/uk/gov/hmcts/reform/pcqconsolidationservice/postdeploy/ConsolidationServiceFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pcqconsolidationservice/postdeploy/ConsolidationServiceFunctionalTest.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ReflectionUtils;
 import uk.gov.hmcts.reform.pcqconsolidationservice.ConsolidationComponent;
