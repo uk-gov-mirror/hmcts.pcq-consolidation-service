4a92bfe456d526390084db103d895116103c2483
Dave Jones <dave.jones@hmcts.net>
2020-05-08 16:41:22 +0100

    PCQ-181 Remove unsed import.

--- src/main/java/uk/gov/hmcts/reform/pcqconsolidationservice/ConsolidationComponent.java
+++ src/main/java/uk/gov/hmcts/reform/pcqconsolidationservice/ConsolidationComponent.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.pcqconsolidationservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
