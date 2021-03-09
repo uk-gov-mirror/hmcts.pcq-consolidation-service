7fd27b6f583a2e1ca5afef768570397039a05779
Dave Jones <dave.jones@hmcts.net>
2021-03-09 05:21:25 +0000

    PCQ-814: Idam down, remove dependancy on idam.

--- src/functionalTest/java/uk/gov/hmcts/reform/pcqconsolidationservice/postdeploy/ConsolidationServiceFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pcqconsolidationservice/postdeploy/ConsolidationServiceFunctionalTest.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
