f790eb4fea8d9410a672d656569260e89c2a8b03
Dave Jones <dave.jones@hmcts.net>
2021-03-09 01:10:29 +0000

    PCQ-814: Idam back reinstate func test.

--- src/functionalTest/java/uk/gov/hmcts/reform/pcqconsolidationservice/postdeploy/ConsolidationServiceFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/pcqconsolidationservice/postdeploy/ConsolidationServiceFunctionalTest.java
@@ -5,7 +5,6 @@ import com.gilecode.reflection.ReflectionAccessor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -80,7 +79,7 @@ public class ConsolidationServiceFunctionalTest extends ConsolidationServiceTest
 
 
     @SuppressWarnings("unchecked")
-    @Ignore
+    @Test
     public void testExecuteMethod() throws IOException, IllegalAccessException {
 
         //Invoke the executor
